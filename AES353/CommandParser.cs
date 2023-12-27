aba5d18f3e08067d07735cb65752130afa2630ec
Navnidhi2475 <148339271+Navnidhi2475@users.noreply.github.com>
2023-12-28 00:49:38 +0530

    Add files via upload

---
+++ AES353/CommandParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+public class CommandParser
+{
+    private TextBox codeTextBox;
+    private PictureBox displayArea;
+
+    // Constructor to link UI controls with the processor
+    public CommandParser(TextBox codeTextBox, PictureBox displayArea)
+    {
+        this.codeTextBox = codeTextBox;
+        this.displayArea = displayArea;
+    }
+
+    // Executes a given instruction from the input
+    public void HandleInstruction(string instruction)
+    {
+        // Define how instructions are handled and executed
+    }
+
+    // Runs the sequence of instructions from the editor
+    public void PerformBatchInstructions()
+    {
+        var instructionSet = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var singleInstruction in instructionSet)
+        {
+            HandleInstruction(singleInstruction.Trim());
+        }
+    }
+
+    // Saves the current set of instructions to a file
+    public void StoreInstructions(string destinationPath)
+    {
+        File.WriteAllText(destinationPath, codeTextBox.Text);
+    }
+
+    // Loads a set of instructions from a file into the editor
+    public void LoadInstructions(string sourcePath)
+    {
+        codeTextBox.Text = File.ReadAllText(sourcePath);
+    }
+
+    // Conducts a syntax check on the instructions in the editor
+    public void InspectSyntax()
+    {
+        var instructionSet = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var singleInstruction in instructionSet)
+        {
+            // Implement the logic for syntax checking
+        }
+        // message if syntax is OK
+        MessageBox.Show("Instruction set is syntactically valid.", "Syntax Inspection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    // Verifies if an individual instruction is syntactically correct
+    private bool CheckInstructionValidity(string instruction)
+    {
+        // need to replace with actual logic for validity
+        return true; // assumes everything is valid for nopw
+    }
+}
