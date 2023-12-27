8d07c5c1fbe6d9d57399bd60b67b161b30b27391
Navnidhi2475 <148339271+Navnidhi2475@users.noreply.github.com>
2023-12-27 23:25:38 +0530

    implemnted gui

---
+++ AES353/Form1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AES353
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form load
+        }
+
+        private void click_Run(object sender, EventArgs e)
+        {
+            // run button
+        }
+
+        private void click_Syntax(object sender, EventArgs e)
+        {
+            // syntax button
+        }
+
+        private void text_Command(object sender, EventArgs e)
+        {
+            // command text box
+        }
+
+        private void picture_Display(object sender, EventArgs e)
+        {
+            // display picture box
+        }
+    }
+}
