caea06eb457c8fd4d77edf012f68ddbe03b3f484
Navnidhi2475 <148339271+Navnidhi2475@users.noreply.github.com>
2023-12-28 23:04:29 +0530

    part 1

--- AES353/Form1.cs
+++ AES353/Form1.cs
@@ -7,35 +7,72 @@ namespace AES353
     public partial class Form1 : Form
     {
         private CommandParser processor;
+
         public Form1()
         {
             InitializeComponent();
             processor = new CommandParser(codeTextBox, displayArea);
+            displayArea.Paint += new PaintEventHandler(displayArea_Paint);
+            commandTextBox.KeyUp += new KeyEventHandler(commandTextBox_KeyUp);
+            runButton.Click += new EventHandler(click_Run);
+            syntaxButton.Click += new EventHandler(click_Syntax);
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Form load
+            // This method is called when the form loads.
         }
 
         private void click_Run(object sender, EventArgs e)
         {
-            // run button
+            try
+            {
+                processor.ExecuteProgram();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error executing the program: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void click_Syntax(object sender, EventArgs e)
         {
-            // syntax button
+            try
+            {
+                processor.CheckSyntax();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Syntax error: " + ex.Message, "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void text_Command(object sender, EventArgs e)
+        private void commandTextBox_KeyUp(object sender, KeyEventArgs e)
         {
-            // command text box
+            if (e.KeyCode == Keys.Enter)
+            {
+                var commandText = commandTextBox.Text;
+                try
+                {
+                    processor.ExecuteCommand(commandText);
+                    commandTextBox.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error executing command: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
-        private void picture_Display(object sender, EventArgs e)
+        private void displayArea_Paint(object sender, PaintEventArgs e)
         {
-            // display picture box
+            processor.SetupGraphics(e);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            processor.Cleanup();
         }
     }
 }
